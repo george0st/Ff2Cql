ac773292d7b481b0d9ddc4004519fd23d2011837
jist <95856749+george0st@users.noreply.github.com>
2024-12-20 20:39:07 +0100

    add connection.json files

--- console_app/Csv2Cql/src/main/java/org/george0st/Main.java
+++ console_app/Csv2Cql/src/main/java/org/george0st/Main.java
@@ -1,16 +1,34 @@
 package org.george0st;
 
 
+import java.io.File;
+
 public class Main {
+
+    private static String setupFile(String[] files){
+
+        for (String file: files)
+            if (new File(file).exists())
+                return file;
+        return null;
+    }
+
     public static void main(String[] args) {
 
 
-        CsvCqlProcessor aa = new CsvCqlProcessor(Setup.getInstance("connection.json"));
-        aa.execute("test.csv");
-        //aa.Connect();
-
-        //aa.Test();
+//        String[] connections=new String[]{"connection-private.json","connection.json"};
+//        String setupFile=null;
 //
+//        for (String connection: connections)
+//            if (new File(connection).exists()) {
+//                setupFile = connection;
+//                break;
+//            }
+
+        String setupFile=Main.setupFile(new String[]{"connection-private.json","connection.json"});
+        CsvCqlProcessor aa = new CsvCqlProcessor(Setup.getInstance(setupFile));
+        aa.execute("test.csv");
+
         System.out.printf("Hello and welcome!");
     }
 }
