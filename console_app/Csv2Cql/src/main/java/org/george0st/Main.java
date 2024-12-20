8a69d351d4383475028767d32060272ba114a5e4
jist <95856749+george0st@users.noreply.github.com>
2024-12-20 20:43:44 +0100

    Update Main.java

--- console_app/Csv2Cql/src/main/java/org/george0st/Main.java
+++ console_app/Csv2Cql/src/main/java/org/george0st/Main.java
@@ -6,7 +6,6 @@ import java.io.File;
 public class Main {
 
     private static String setupFile(String[] files){
-
         for (String file: files)
             if (new File(file).exists())
                 return file;
@@ -14,16 +13,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-
-
-//        String[] connections=new String[]{"connection-private.json","connection.json"};
-//        String setupFile=null;
-//
-//        for (String connection: connections)
-//            if (new File(connection).exists()) {
-//                setupFile = connection;
-//                break;
-//            }
 
         String setupFile=Main.setupFile(new String[]{"connection-private.json","connection.json"});
         CsvCqlProcessor aa = new CsvCqlProcessor(Setup.getInstance(setupFile));
