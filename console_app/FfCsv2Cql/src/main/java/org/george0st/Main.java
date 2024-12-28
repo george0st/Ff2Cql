e8970b9b04db26c56af1c3bb3a2a81cdbfa9c492
jist <95856749+george0st@users.noreply.github.com>
2024-12-28 11:39:21 +0100

    cleaning

--- console_app/FfCsv2Cql/src/main/java/org/george0st/Main.java
+++ console_app/FfCsv2Cql/src/main/java/org/george0st/Main.java
@@ -1,28 +1,17 @@
 package org.george0st;
 
-
-import com.datastax.oss.driver.api.core.ProtocolVersion;
-import com.datastax.oss.driver.api.core.type.codec.TypeCodecs;
 import com.opencsv.exceptions.CsvValidationException;
 import org.george0st.helper.ReadableTime;
 import org.george0st.helper.Setup;
 import org.george0st.processor.CsvCqlWrite;
 import picocli.CommandLine;
-
-import java.io.File;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 
-@Command(name = "example", mixinStandardHelpOptions = true, version = "FfCsv2Cql 1.2", description = "asdasda")
+@Command(name = "example", mixinStandardHelpOptions = true, version = "FfCsv2Cql 1.2", description = "A simple transfer data from NiFi FileFlow to CQL.")
 public class Main implements Runnable {
 
     @Option(names = { "-c", "--config" },
@@ -41,20 +30,25 @@ public class Main implements Runnable {
     public void run() {
 
         try {
+            // define setup
             Setup setup = Setup.getInstance(config);
             setup.setBulk(bulk);
 
+            // general access
             CqlAccess access=new CqlAccess(setup);
             long finish, start;
 
+            //  processing files
             for (String inputFile : inputFiles) {
                 start = System.currentTimeMillis();
+
+                //  write file
                 CsvCqlWrite write = new CsvCqlWrite(access);
                 write.execute(inputFile);
+
                 finish = System.currentTimeMillis();
                 System.out.println("File '" + inputFile + "': "+ ReadableTime.fromMillisec(finish - start) +
                         "(" + (finish-start) +" ms)");
-
             }
         }
         catch(Exception ex) {
@@ -63,11 +57,6 @@ public class Main implements Runnable {
     }
 
     public static void main(String[] args) throws CsvValidationException, IOException {
-
-        // https://github.com/timtiemens/javacommandlineparser
-        // https://github.com/remkop/picocli
-        // https://jcommander.org/
-
         int exitCode = new CommandLine(new Main()).execute(args);
         System.exit(exitCode);
     }
