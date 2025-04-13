218dfe4499631565a4bf56844c77647ad61e063a
jist <95856749+george0st@users.noreply.github.com>
2025-04-13 15:15:09 +0200

    Update GetCQLPerformance.java

--- nifi/cql-processor/nifi-cql-processors/src/test/java/org/george0st/processors/cql/GetCQLPerformance.java
+++ nifi/cql-processor/nifi-cql-processors/src/test/java/org/george0st/processors/cql/GetCQLPerformance.java
@@ -174,4 +174,59 @@ public class GetCQLPerformance extends GetCQLBase {
         }
     }
 
+    @Test
+    public void testRead100KMultipleWithContent() throws Exception {
+        String content = "\"colbigint\",\"colint\",\"coltext\",\"colfloat\",\"coldouble\",\"coldate\",\"coltime\",\"coltimestamp\",\"colboolean\",\"coluuid\",\"colsmallint\",\"coltinyint\",\"coltimeuuid\",\"colvarchar\"\n" +
+                "\"0\",\"1064\",\"zeVOKGnORq\",\"627.6811\",\"395.8522407512559\",\"1971-11-12\",\"03:37:15\",\"2000-09-25T22:18:45Z\",\"false\",\"6080071f-4dd1-4ea5-b711-9ad0716e242a\",\"8966\",\"55\",\"f45e58f5-c3b7-11ef-8d19-97ae87be7c54\",\"Tzxsw\"\n" +
+                "\"1\",\"1709\",\"7By0z5QEXh\",\"652.03955\",\"326.9081263857284\",\"2013-12-17\",\"08:43:09\",\"2010-04-27T07:02:27Z\",\"false\",\"7d511666-2f81-41c4-9d5c-a5fa87f7d1c3\",\"24399\",\"38\",\"f45e8006-c3b7-11ef-8d19-172ff8d0d752\",\"exAbN\"\n";
+        List<MockFlowFile> results;
+
+        // Prepare data for performance test
+        PutCQLPerformance putCQL = new PutCQLPerformance();
+        putCQL.init();
+        putCQL.csvRandomWrite100K();
+
+        //  sleep before read (2 seconds)
+        Thread.sleep(2000);
+
+        // Read data
+        for (TestSetupRead setup : setups) {
+            setup.columnNames="colbigint, colint";
+
+            results = runTestParallel(setup, content, 5);
+
+            //  check amount of read items
+            for (MockFlowFile result: results) {
+                assertNotNull(result, String.format("Issue with processing in '%s'", setup.name));
+                assertEquals(100_000, Long.parseLong(result.getAttribute(CQLAttributes.READ_COUNT)));
+            }
+        }
+    }
+
+    @Test
+    public void testRead100KMultiple() throws Exception {
+        List<MockFlowFile> results;
+
+        // Prepare data for performance test
+        PutCQLPerformance putCQL = new PutCQLPerformance();
+        putCQL.init();
+        putCQL.csvRandomWrite100K();
+
+        //  sleep before read (2 seconds)
+        Thread.sleep(2000);
+
+        // Read data
+        for (TestSetupRead setup : setups) {
+            setup.columnNames="colbigint, colint";
+
+            results = runTestParallel(setup, 5);
+
+            //  check amount of read items
+            for (MockFlowFile result: results) {
+                assertNotNull(result, String.format("Issue with processing in '%s'", setup.name));
+                assertEquals(100_000, Long.parseLong(result.getAttribute(CQLAttributes.READ_COUNT)));
+            }
+        }
+    }
+
 }
