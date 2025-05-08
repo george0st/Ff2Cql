c079e8b1f6375c0cb3340bb497d8fc2f68001048
george0st <steuer.jiri@gmail.com>
2025-05-08 17:59:26 +0200

    Update docker images

--- nifi/cql-processor/dockers/nifi.sh
+++ nifi/cql-processor/dockers/nifi.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# get/run '2.4.0' nifi
+
 docker network create testnet
 docker pull apache/nifi:2.4.0
 docker run --name nifi -p 8443:8443 -d --network testnet -e SINGLE_USER_CREDENTIALS_USERNAME=test -e SINGLE_USER_CREDENTIALS_PASSWORD=testUserXNifiv2 apache/nifi:2.4.0
--- nifi/cql-processor/dockers/yugabyte.sh
+++ nifi/cql-processor/dockers/yugabyte.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# get/run '2024.2.2.2-b2' yugabyte
+
 docker network create testnet
 docker pull yugabytedb/yugabyte:2024.2.2.2-b2
 docker run --name yugabyte -p 9042:9042 -p 7199:7199 -d --network testnet yugabytedb/yugabyte:2024.2.2.2-b2
