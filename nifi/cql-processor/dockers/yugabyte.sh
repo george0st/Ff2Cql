a58b0728d9dcb16f679b458789654dc8817f78bc
george0st <steuer.jiri@gmail.com>
2025-05-08 18:56:32 +0200

    Update yugabyte.sh

--- nifi/cql-processor/dockers/yugabyte.sh
+++ nifi/cql-processor/dockers/yugabyte.sh
@@ -7,11 +7,11 @@ docker pull yugabytedb/yugabyte:2.25.1.0-b381
 docker run --name yugabyte -p 7000:7000 -p 9000:9000 -p 15433:15433 -p 5433:5433 -p 9042:9042 -d --network testnet yugabytedb/yugabyte:2.25.1.0-b381 bin/yugabyted start --background=false
 
 
-# show status
+# 1. show status
 # docker exec -it yugabyte yugabyted status
 
-# run ysqlsh
+# 2. run ysqlsh
 # docker exec -it yugabyte bash -c "bin/ysqlsh -h c82cf988b153  -U yugabyte -d yugabyte"
 
-# web UI
+# 3. web UI
 # http://localhost:15433/
