231c2ada45ee8d98572313544b9bd7ccbcd2ca67
george0st <steuer.jiri@gmail.com>
2025-05-08 18:26:54 +0200

    Update nifi.sh

--- nifi/cql-processor/dockers/nifi.sh
+++ nifi/cql-processor/dockers/nifi.sh
@@ -5,3 +5,7 @@
 docker network create testnet
 docker pull apache/nifi:2.4.0
 docker run --name nifi -p 8443:8443 -d --network testnet -e SINGLE_USER_CREDENTIALS_USERNAME=test -e SINGLE_USER_CREDENTIALS_PASSWORD=testUserXNifiv2 apache/nifi:2.4.0
+
+# https://localhost:8443/nifi/
+# username: test
+# pwd:      testUserXNifiv2
