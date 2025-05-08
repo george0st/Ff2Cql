dfeabd3b99aa74f3ad458f80106ccc6394c36c8b
george0st <steuer.jiri@gmail.com>
2025-05-08 22:51:11 +0200

    Update yugabyte.sh

--- nifi/cql-processor/dockers/yugabyte.sh
+++ nifi/cql-processor/dockers/yugabyte.sh
@@ -10,8 +10,11 @@ docker run --name yugabyte -p 7000:7000 -p 9000:9000 -p 15433:15433 -p 5433:5433
 # 1. show status
 # docker exec -it yugabyte yugabyted status
 
-# 2. run ysqlsh
-# docker exec -it yugabyte bash -c "bin/ysqlsh -h c82cf988b153  -U yugabyte -d yugabyte"
+# 2. run ycqlsh (CQL)
+# docker exec -it yugabyte bash -c "bin/ycqlsh yugabyte 9042 -u cassandra"
 
 # 3. web UI
 # http://localhost:15433/
+
+# 4. run ysqlsh (SQL)
+# docker exec -it yugabyte bash -c "bin/ysqlsh -h yugabyte -U yugabyte -d yugabyte"
