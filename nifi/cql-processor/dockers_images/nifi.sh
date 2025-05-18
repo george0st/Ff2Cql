51ffa83e11093268cb6a0ebe120308b37f6e7d7f
george0st <steuer.jiri@gmail.com>
2025-05-18 18:05:59 +0200

    Update nifi.sh

--- nifi/cql-processor/dockers_images/nifi.sh
+++ nifi/cql-processor/dockers_images/nifi.sh
@@ -6,6 +6,15 @@ docker network create testnet
 docker pull apache/nifi:2.4.0
 docker run --name nifi -p 8443:8443 -d --network testnet -e SINGLE_USER_CREDENTIALS_USERNAME=test -e SINGLE_USER_CREDENTIALS_PASSWORD=testUserXNifiv2 apache/nifi:2.4.0
 
+
+# Usage
+########
+
+# 1. open nifi in browner
 # https://localhost:8443/nifi/
 # username: test
 # pwd:      testUserXNifiv2
+
+# 2. use 'nifi-cql-nar-*.nar' in nifi
+# 2.1 write the '*.nar' file to directory '/opt/nifi/nifi-current/lib'
+# 2.2 restart nifi
